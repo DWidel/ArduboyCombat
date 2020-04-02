6198f857d163ff5583cb347e2455207d3308e9c9
DWidel <49377673+DWidel@users.noreply.github.com>
2020-04-02 10:06:11 -0400

    Update Machine.h
    Removed comments I didn't write.

--- Machine.h
+++ Machine.h
@@ -133,25 +133,13 @@ Arduboy2Base arduboy;
   short T2;
   short M; //Temporary location
 
-
-  // The carry flag is set if the last operation caused an overflow from bit 7 of the result or an underflow from bit 0. 
-  // This condition Is Set during arithmetic, comparison And during logical shifts. It can be explicitly Set Using the 'Set Carry Flag' (SEC) instruction and cleared with 'Clear Carry Flag' (CLC).
-  bool fCarry;
-  // The zero flag is set if the result of the last operation as was zero.
+  bool fCarry; 
   bool fZero;
-  // The interrupt disable flag is set if the program has executed a 'Set Interrupt Disable' (SEI) instruction. While this flag is set the processor 
-  // will not respond to interrupts from devices until it is cleared by a 'Clear Interrupt Disable' (CLI) instruction.
   bool fIntDis = true;
-  // While the decimal mode flag is set the processor will obey the rules of Binary Coded Decimal (BCD) arithmetic during addition and subtraction. 
-  // The flag can be explicitly Set Using 'Set Decimal Flag' (SED) and cleared with 'Clear Decimal Flag' (CLD).
-  bool fDecMod;
-  // The break command bit is set when a BRK instruction has been executed and an interrupt has been generated to process it.
-  bool fBreak;
-  // The overflow flag is set during arithmetic operations if the result has yielded an invalid 2's complement result (e.g. adding to positive numbers and 
-  // ending up With a negative result: 64 + 64 => -128). It is determined by looking at the carry between bits 6 and 7 and between bit 7 and the carry flag.
-  bool fOverflow;
-  // The negative flag is set if the result of the last operation had bit 7 set to a one.
-  bool fNeg; 
+   bool fDecMod;
+   bool fBreak;
+    bool fOverflow;
+   bool fNeg; 
     
  
 
